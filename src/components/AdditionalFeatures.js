1d938010941b6b1d0774401d77664d1efc4cd192
thericktastic <love@The-RT-MBP.attlocal.net>
2020-01-22 21:34:13 -0500

    MVP

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+// components
 import AdditionalFeature from './AdditionalFeature';
 
+
 const AdditionalFeatures = props => {
+  console.log("This is props in AdditionalFeaturess: ", props)
   return (
     <div className="content">
       <h4>Additional Features</h4>
-      {props.additionalFeatures.length ? (
+      {props.additionalFeaturesOnProps.length ? (
         <ol type="1">
-          {props.additionalFeatures.map(item => (
+          {props.additionalFeaturesOnProps.map(item => (
             <AdditionalFeature key={item.id} feature={item} />
           ))}
         </ol>
@@ -18,4 +23,22 @@ const AdditionalFeatures = props => {
   );
 };
 
-export default AdditionalFeatures;
+// export default AdditionalFeatures;
+
+
+// Step 2 - connect the components
+// Use the connect() function to map out state from the Redux store to the component props
+// Step 2a - wrap the component in the second connect() function call
+// Step 2b - first function call takes in a function and an object
+// Step 2b.i - the function we pass is usually named mapStateToProps
+
+const mapStateToProps = state => {
+  return {
+    additionalFeaturesOnProps: state.additionalFeatures
+  }
+}
+
+export default connect(
+  mapStateToProps, // function
+  {} // object
+)(AdditionalFeatures);
--- src/components/Total.js
+++ src/components/Total.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
 
 const Total = props => {
   return (
     <div className="content">
-      <h4>Total Amount: ${props.car.price + props.additionalPrice}</h4>
+      <h4>Total Amount: ${props.priceOnProps + props.additionalPriceOnProps}</h4>
     </div>
   );
 };
 
-export default Total;
+// export default Total;
+
+// Step 2 - connect the components
+// Use the connect() function to map out state from the Redux store to the component props
+// Step 2a - wrap the component in the second connect() function call
+// Step 2b - first function call takes in a function and an object
+// Step 2b.i - the function we pass is usually named mapStateToProps
+
+const mapStateToProps = state => {
+  return {
+    priceOnProps: state.car.price,
+    additionalPriceOnProps: state.additionalPrice
+  };
+};
+
+export default connect(
+  mapStateToProps, // function
+  {} // object
+)(Total);
